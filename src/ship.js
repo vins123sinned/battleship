3f163876cb6430b4a001da6005e9875858193282
Vinson 2 <vinsonhe2@gmail.com>
2025-04-29 19:40:58 -0700

    Add hit and isSunk methods to Ship class

--- src/ship.js
+++ src/ship.js
@@ -1,7 +1,16 @@
 export class Ship {
     constructor(length) {
         this.length = length;
-        this.hit = 0;
+        this.hitTimes = 0;
         this.sunk = false;
+    }
+
+    hit() {
+        this.hitTimes += 1;
+    }
+
+    isSunk() {
+        if (this.hitTimes === this.length) this.sunk = true;
+        return this.sunk;
     }
 }
